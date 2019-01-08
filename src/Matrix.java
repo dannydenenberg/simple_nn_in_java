5a8a077e374642f122d8678b840da66c08f804b5
Danny Denenberg <dannydenenberg@gmail.com>
2019-01-08 06:32:32 -0600

    matrix initializers

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,62 @@
+import java.util.ArrayList;
+
 public class Matrix {
+    private double[][] mat;
+
+    /**
+     * Initialization functions
+     */
+
+    public Matrix(double[][] matrix) {
+        mat = matrix;
+    }
+
+    public static Matrix zeros(Shape s) {
+        return new Matrix(fillShapeWithValue(s, 0));
+    }
+
+    public static Matrix ones(Shape s) {
+        return new Matrix(fillShapeWithValue(s, 1));
+    }
+
+    public static Matrix tens(Shape s) {
+        return new Matrix(fillShapeWithValue(s, 10));
+    }
+
+    public static Matrix fillWithVal(Shape s, double value) {
+        return new Matrix(fillShapeWithValue(s, value));
+    }
+
+    public static Matrix random(Shape s) {
+        double[][] matrix = new double[s.rows][s.cols];
+
+        for (int i = 0; i < s.rows; i++) {
+            for (int j = 0; j < s.cols; j++) {
+                matrix[i][j] = Math.random();
+            }
+        }
+
+        return new Matrix(matrix);
+    }
+
+    public static double[][] fillShapeWithValue(Shape s, double val) {
+        double[][] matrix = new double[s.rows][s.cols];
+
+        for (int i = 0; i < s.rows; i++) {
+            for (int j = 0; j < s.cols; j++) {
+                matrix[i][j] = val;
+            }
+        }
+
+        return matrix;
+    }
+
+    /***************************************************************************/
+
+
+
+
     
+
+
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,7 +1,15 @@
+/**
+ * This class will be used to represent the shape of a matrix.
+ */
 public class Shape {
     public int rows;
     public int cols;
 
+    /**
+     * Initialization is pretty straightforward. Just enter the shape (rows, columns).
+     * @param r
+     * @param c
+     */
     public Shape(int r, int c) {
         rows = r;
         cols = c;
